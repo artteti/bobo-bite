1d0037f93abf8b9a92a62f45b516ae063cec1b4b
Tetiana <83167908+artteti@users.noreply.github.com>
2024-10-22 13:17:45 +0200

    Styled Components Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>BoboBite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div>
+        <h1>Welcome to BoboBite</h1>
+        <Button>Click Me</Button>
       </div>
     </>
   );
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #4caf50;
+  color: white;
+  padding: 15px 32px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+export default Button;
