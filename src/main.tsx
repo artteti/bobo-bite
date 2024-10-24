f18db27d2bcc91845de07f202b1303036b21dd5a
Tetiana <83167908+artteti@users.noreply.github.com>
2024-10-24 12:37:56 +0200

    i18next instal

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./store.ts";
 import App from "./App.tsx";
 import "./index.css";
-
+import "./utils/i18n.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import Button from "../components/Button";
 import { LoginForm } from "../components/LogInForm";
+import { useTranslation } from "react-i18next";
 
 export const HomePage = () => {
+  const { t } = useTranslation();
   return (
     <>
-      <h1>Welcome to BoboBite</h1>
+      <h1>{t("welcomeMessage")}</h1>
       <LoginForm />
       <Button>Click Me</Button>
     </>
